95777000db346caf1a9214492dcb88db6d3dfc10
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2024-02-06 17:22:57 +0100

    modified mutation createReview and added delete

--- backend/src/resolvers/Review.ts
+++ backend/src/resolvers/Review.ts
@@ -1,48 +1,51 @@
 import { Resolver, Query, Mutation, Arg, Int, Ctx } from 'type-graphql';
 import { Review } from '../entities/review';
 import { UserContext } from './User';
+import { User } from '../entities/user';
 
 @Resolver()
 export class ReviewResolver {
   @Query(() => [Review])
   async reviews(): Promise<Review[]> {
-    try {
-      const reviews = await Review.find();
-      return reviews;
-    } catch (error) {
-      throw new Error('Failed to fetch reviews: ' + error.message);
-    }
+    return await Review.find();
   }
+
   @Query(() => [Review])
   async reviewsForUser(
     @Arg('userId', () => Int) userId: number
   ): Promise<Review[]> {
+    return await Review.find({ where: { userId } });
+  }
+
+  @Mutation(() => Boolean)
+  async deleteReview(
+    @Arg('reviewId', () => Int) reviewId: number
+  ): Promise<Boolean> {
     try {
-      const reviews = await Review.find({ where: { userId } });
-      return reviews;
+      const review = await Review.findOne({ where: { id: reviewId } });
+      if (!review) throw new Error('Review not found!');
+      await Review.remove(review);
+      return true;
     } catch (error) {
-      throw new Error('Failed to fetch reviews for user: ' + error.message);
+      console.error('Failed to delete review:', error);
+      return false;
     }
   }
+
   @Mutation(() => Review)
   async createReview(
     @Arg('rating', () => Int) rating: number,
     @Arg('comment') comment: string,
     @Ctx() ctx: UserContext
   ): Promise<Review> {
-    if (!ctx.user) {
-      throw new Error('Not authenticated!');
-    }
-
-    try {
-      const review = new Review();
-      review.rating = rating;
-      review.comment = comment;
-      review.user = ctx.user;
-      await review.save();
-      return review;
-    } catch (error) {
-      throw new Error('Failed to create review: ' + error.message);
-    }
+    if (!ctx.user) throw new Error('Not authenticated!');
+    const user = await User.findOne({ where: { id: ctx.user.id } });
+    if (!user) throw new Error('User not found!');
+    const review = new Review();
+    review.rating = rating;
+    review.comment = comment;
+    review.user = user;
+    await review.save();
+    return review;
   }
 }
