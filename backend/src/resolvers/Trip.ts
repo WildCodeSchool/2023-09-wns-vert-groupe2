502284eff2edd99d3ef2273103a4f2f2bef6bb5f
Mohamed <mohamed.berkoussi@davi.ai>
2024-02-06 11:24:02 +0100

    Update: Adding context and try-catch blocks

--- backend/src/resolvers/Trip.ts
+++ backend/src/resolvers/Trip.ts
@@ -1,31 +1,55 @@
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
+import { Resolver, Mutation, Arg, Query, Ctx } from "type-graphql";
 import { Trip } from "../entities/trip";
 import { TripInput } from "../inputs/Trip";
 import { TripUpdateInput } from "../inputs/TripUpdate";
-import { User } from "../entities/user";
+// import { User } from "../entities/user";
+import { UserContext } from "../types/User";
 
 @Resolver()
 export class TripResolver {
   @Query(() => [Trip])
   async trips(): Promise<Trip[]> {
-    // Ici c pour récupérer tous les voyages depuis la base de données
-    return Trip.find();
+    try {
+      // Ici c pour récupérer tous les voyages depuis la base de données
+      return Trip.find();
+    } catch (error) {
+      console.error(
+        "Une erreur s'est produite lors de la récupération des voyages :",
+        error
+      );
+      throw error;
+    }
   }
 
   // Ici c pour créer un nouveau voyage avec les données fournies.
   @Mutation(() => Trip)
-  async createTrip(@Arg("data") data: TripInput): Promise<Trip> {
-    const trip = Trip.create({
-      date: data.date,
-      price: data.price,
-      status: data.status,
-      startLocation: data.startLocation,
-      stopLocations: data.stopLocations,
-      endLocation: data.endLocation,
-      passengers: [],
-    });
-    trip.passengers = await User.findByIds(data.passengers);
-    return await trip.save();
+  async createTrip(
+    @Arg("data") data: TripInput,
+    @Ctx() ctx: UserContext
+  ): Promise<Trip> {
+    if (!ctx.user) {
+      throw new Error("Not authenticated!");
+    }
+    try {
+      const trip = Trip.create({
+        date: data.date,
+        price: data.price,
+        status: data.status,
+        startLocation: data.startLocation,
+        stopLocations: data.stopLocations,
+        endLocation: data.endLocation,
+        passengers: [],
+        driver: ctx.user.id,
+      });
+      // trip.passengers = await User.findByIds(data.passengers);
+      return await trip.save();
+    } catch (error) {
+      console.error(
+        "Une erreur s'est produite lors de la création du voyage :",
+        error
+      );
+      throw error;
+    }
   }
 
   // Ici c pour mettre à jour un voyage existant
@@ -34,26 +58,42 @@ export class TripResolver {
     @Arg("id") id: number,
     @Arg("data") data: TripUpdateInput
   ): Promise<Trip | null> {
-    const trip = await Trip.findOne({ where: { id } });
+    try {
+      const trip = await Trip.findOne({ where: { id } });
 
-    if (!trip) {
-      return null;
+      if (!trip) {
+        return null;
+      }
+
+      Object.assign(trip, data);
+
+      await trip.save();
+
+      return trip;
+    } catch (error) {
+      console.error(
+        "Une erreur s'est produite lors de la mise à jour du voyage :",
+        error
+      );
+      throw error;
     }
-
-    Object.assign(trip, data);
-
-    await trip.save();
-
-    return trip;
   }
 
   // Ici c pour supprimer un voyage
   @Mutation(() => Boolean)
   async deleteTrip(@Arg("id") id: number): Promise<boolean> {
-    const trip = await Trip.findOne({ where: { id } });
-    if (!trip) return false;
+    try {
+      const trip = await Trip.findOne({ where: { id } });
+      if (!trip) return false;
 
-    await trip.remove();
-    return true;
+      await trip.remove();
+      return true;
+    } catch (error) {
+      console.error(
+        "Une erreur s'est produite lors de la suppression du voyage :",
+        error
+      );
+      throw error;
+    }
   }
 }
