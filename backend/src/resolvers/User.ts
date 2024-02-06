f251045275e2eec8b31cc73ab2505bb0e4c0c68c
brithe00 <bthellier51@gmail.com>
2024-02-06 11:31:09 +0100

    added try catch

--- backend/src/resolvers/User.ts
+++ backend/src/resolvers/User.ts
@@ -9,56 +9,64 @@ import { Arg, Ctx, Mutation, ObjectType, Query, Resolver } from 'type-graphql';
 export class UserResolver {
 	@Mutation(() => AuthenticatedUser)
 	async register(@Arg('input') input: UserRegisterInput) {
-		const alreadyExists = await User.findOne({
-			where: {
+		try {
+			const alreadyExists = await User.findOne({
+				where: {
+					email: input.email,
+				},
+			});
+
+			if (alreadyExists) {
+				throw new Error('User already exists !');
+			}
+
+			const user = await User.save({
 				email: input.email,
-			},
-		});
+				password: await hashPassword(input.password),
+				firstname: '',
+				lastname: '',
+				description: '',
+				pictureUrl: '',
+				birthdate: new Date(),
+				phoneNumber: '',
+			});
 
-		if (alreadyExists) {
-			throw new Error('User already exists !');
+			const token = createJwt(user);
+
+			return { user, token };
+		} catch (error) {
+			throw new Error('Failed to register: ' + error.message);
 		}
-
-		const user = await User.save({
-			email: input.email,
-			password: await hashPassword(input.password),
-			firstname: '',
-			lastname: '',
-			description: '',
-			pictureUrl: '',
-			birthdate: new Date(),
-			phoneNumber: '',
-		});
-
-		const token = createJwt(user);
-
-		return { user, token };
 	}
 
 	@Mutation(() => AuthenticatedUser)
 	async login(@Arg('input') input: UserLoginInput) {
-		const user = await User.findOne({
-			where: {
-				email: input.email,
-			},
-		});
+		try {
+			const user = await User.findOne({
+				where: {
+					email: input.email,
+				},
+			});
 
-		if (!user) {
-			throw new Error('User not found!');
+			if (!user) {
+				throw new Error('User not found!');
+			}
+
+			const passwordIsValid = await comparePasswords(
+				user.password,
+				input.password
+			);
+
+			if (!passwordIsValid) {
+				throw new Error('Invalid credentials !');
+			}
+
+			const token = createJwt(user);
+
+			return { user, token };
+		} catch (error) {
+			throw new Error('Failed to login: ' + error.message);
 		}
-
-		const passwordIsValid = await comparePasswords(
-			user.password,
-			input.password
-		);
-
-		if (!passwordIsValid) {
-			throw new Error('Invalid credentials !');
-		}
-
-		const token = createJwt(user);
-
-		return { user, token };
 	}
 
 	@Query(() => User)
@@ -67,12 +75,16 @@ export class UserResolver {
 			throw new Error('Not authenticated !');
 		}
 
-		const authenticatedUser = await User.findOne({
-			where: {
-				id: ctx.user.id,
-			},
-		});
+		try {
+			const authenticatedUser = await User.findOne({
+				where: {
+					id: ctx.user.id,
+				},
+			});
 
-		return authenticatedUser;
+			return authenticatedUser;
+		} catch (error) {
+			throw new Error('Failed to fetch my informations: ' + error.message);
+		}
 	}
 }
