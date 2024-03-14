382be4b75a38a06083b9273d0762b47c76112ee9
Brian <61732049+brithe00@users.noreply.github.com>
2024-03-14 16:33:37 +0100

    Revert "Test unitaire moha"

--- backend/__tests__/hello.test.ts
+++
@@ -1,6 +0,0 @@
-describe('👏 - Test - Hello World !', () => {
-	it('Retourner "Hello World !"', () => {
-		const str = 'Hello World !';
-		expect(str).toBe('Hello World !');
-	});
-});
--- backend/__tests__/trip.test.ts
+++
@@ -1,8 +0,0 @@
-import { Trip } from "../src/entities/trip";
-
-describe("Trip Entity", () => {
-  it("should create a new instance of Trip", () => {
-    const trip = new Trip();
-    expect(trip).toBeInstanceOf(Trip);
-  });
-});
