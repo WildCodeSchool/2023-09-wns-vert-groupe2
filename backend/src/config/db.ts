d9a3224b448e5c8846b1ca4bbef2aa19d374149b
Adnan <adbak.dev@gmail.com>
2024-04-18 15:15:40 +0200

    test deployment

---
+++ backend/src/config/db.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "example",
+  database: "postgres",
+  entities: [`${__dirname}/../entities/*{.js,.ts}`],
+
+  synchronize: true,
+  logging: ["query", "error"],
+});
+
+export default dataSource;
