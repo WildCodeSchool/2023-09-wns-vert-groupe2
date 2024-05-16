000a938ea826e229d515ae9ed2fa0ea20031d93a
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2024-05-16 16:08:41 +0200

    adding cache to reviewforUser

--- backend/src/resolvers/Review.ts
+++ backend/src/resolvers/Review.ts
@@ -3,71 +3,83 @@ import { Review } from '../entities/review';
 import { UserContext } from '../types/User';
 import { User } from '../entities/user';
 import { checkIfRegistered } from '../utils/checker';
+import { redisClient } from '../index';
 
 @Resolver()
 export class ReviewResolver {
-	@Query(() => [Review])
-	async reviews(): Promise<Review[]> {
-		return await Review.find({ relations: ['author', 'target'] });
-	}
+  @Query(() => [Review])
+  async reviews(): Promise<Review[]> {
+    return await Review.find({ relations: ['author', 'target'] });
+  }
 
-	@Query(() => [Review])
-	async reviewsForUser(
-		@Arg('userId', () => Int) userId: number
-	): Promise<Review[]> {
-		return await Review.find({
-			where: {
-				target: {
-					id: userId,
-				},
-			},
-			relations: ['author', 'target'],
-		});
-	}
+  @Query(() => [Review])
+  async reviewsForUser(
+    @Arg('userId', () => Int) userId: number
+  ): Promise<Review[]> {
+    const redisKey = `user:${userId}:reviews`;
 
-	@Mutation(() => Boolean)
-	async deleteReview(
-		@Arg('reviewId', () => Int) reviewId: number,
-		@Ctx() ctx: UserContext
-	): Promise<Boolean> {
-		try {
-			checkIfRegistered(ctx.user);
-			const review = await Review.findOne({ where: { id: reviewId } });
-			if (!review) throw new Error('Review not found!');
+    const cachedReviews = await redisClient.get(redisKey);
+    if (cachedReviews !== null) {
+      return JSON.parse(cachedReviews);
+    } else {
+      const reviews = await Review.find({
+        where: {
+          target: {
+            id: userId,
+          },
+        },
+        relations: ['author', 'target'],
+      });
 
-			if (review.author.id !== ctx.user.id) {
-				throw new Error('You are not authorized to delete this review!');
-			}
+      await redisClient.set(redisKey, JSON.stringify(reviews), { EX: 3600 });
 
-			await Review.remove(review);
-			return true;
-		} catch (error) {
-			console.error('Failed to delete review:', error);
-			return false;
-		}
-	}
+      return reviews;
+    }
+  }
 
-	@Mutation(() => Review)
-	async createReview(
-		@Arg('rating', () => Int) rating: number,
-		@Arg('comment') comment: string,
-		@Arg('targetId', () => Int) targetId: number,
-		@Ctx() ctx: UserContext
-	): Promise<Review> {
-		checkIfRegistered(ctx.user);
-		const me = await User.findOne({ where: { id: ctx.user.id } });
-		if (!me) throw new Error('User not found!');
+  @Mutation(() => Boolean)
+  async deleteReview(
+    @Arg('reviewId', () => Int) reviewId: number,
+    @Ctx() ctx: UserContext
+  ): Promise<Boolean> {
+    try {
+      checkIfRegistered(ctx.user);
+      const review = await Review.findOne({ where: { id: reviewId } });
+      if (!review) throw new Error('Review not found!');
 
-		const targetUser = await User.findOne({ where: { id: targetId } });
-		if (!targetUser) throw new Error('User not found!');
+      if (review.author.id !== ctx.user.id) {
+        throw new Error('You are not authorized to delete this review!');
+      }
 
-		const review = new Review();
-		review.rating = rating;
-		review.comment = comment;
-		review.author = me;
-		review.target = targetUser;
+      await Review.remove(review);
+      return true;
+    } catch (error) {
+      console.error('Failed to delete review:', error);
+      return false;
+    }
+  }
 
-		await review.save();
-		return review;
-	}
+  @Mutation(() => Review)
+  async createReview(
+    @Arg('rating', () => Int) rating: number,
+    @Arg('comment') comment: string,
+    @Arg('targetId', () => Int) targetId: number,
+    @Ctx() ctx: UserContext
+  ): Promise<Review> {
+    checkIfRegistered(ctx.user);
+    const me = await User.findOne({ where: { id: ctx.user.id } });
+    if (!me) throw new Error('User not found!');
+
+    const targetUser = await User.findOne({ where: { id: targetId } });
+    if (!targetUser) throw new Error('User not found!');
+
+    const review = new Review();
+    review.rating = rating;
+    review.comment = comment;
+    review.author = me;
+    review.target = targetUser;
+
+    await review.save();
+    return review;
+  }
 }
