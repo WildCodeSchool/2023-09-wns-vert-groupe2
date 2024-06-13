418706e9d769f44296b48219d08824399e001879
Mohamed <mohamed.berkoussi@davi.ai>
2024-06-13 15:03:05 +0200

    It's good

--- frontend/src/pages/journeys/create.tsx
+++ frontend/src/pages/journeys/create.tsx
@@ -1,9 +1,11 @@
+import CreateTrip from "@/components/CreateTrip";
 import Layout from "@/components/Layout";
 
 export default function CreateJourney() {
   return (
     <Layout>
       <p>Nouveau trajet</p>
+      <CreateTrip />
     </Layout>
   );
 }
