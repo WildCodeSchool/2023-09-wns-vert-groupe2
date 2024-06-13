8010f7c7fe38bd2bc197d8e77014e23155577566
brian <brian@brian.local>
2024-06-13 16:33:21 +0200

    removed import

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,7 +5,6 @@ import {
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 import type { AppProps } from "next/app";
 import ThemeProvider from "../styles/theme";
 import { setContext } from "@apollo/client/link/context";
