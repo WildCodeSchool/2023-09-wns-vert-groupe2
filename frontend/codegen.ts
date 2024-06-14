4fe65859c9127a3ba6b418114105adf96374ab3a
Josué <josue@figue.io>
2024-06-14 11:19:25 +0200

    ✨ feat(Trips): All Trip Pages
    ajout de toute les querys liés aux trajets, ajout du authprovider

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "http://localhost:4000",
-  documents: ["src/graphql/*.gql"],
+  //documents: ["src/graphql/*.gql"],
   generates: {
     "src/gql/": {
       preset: "client",
